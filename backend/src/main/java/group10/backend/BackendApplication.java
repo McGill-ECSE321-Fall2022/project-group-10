b1086e260de381bc203f2c15ab91a4fbdceef194
Charles Spencer Blancas <86479293+Charles-Spencer-Blancas@users.noreply.github.com>
2022-10-20 20:21:26 -0400

    Initialized Spring project with JPA and postgres drivers

---
+++ backend/src/main/java/group10/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package group10.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/group10/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package group10.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
