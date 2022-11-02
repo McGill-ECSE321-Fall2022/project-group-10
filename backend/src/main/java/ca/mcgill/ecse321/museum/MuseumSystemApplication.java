21b6a73456b0f29458a0883d089f01175be7c31f
Charles Spencer Blancas <86479293+Charles-Spencer-Blancas@users.noreply.github.com>
2022-11-02 14:12:39 -0400

    Added root RequestMapping

--- backend/src/main/java/ca/mcgill/ecse321/museum/MuseumSystemApplication.java
+++ backend/src/main/java/ca/mcgill/ecse321/museum/MuseumSystemApplication.java
@@ -7,12 +7,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import ca.mcgill.ecse321.museum.repository.MuseumSystemRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class MuseumSystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MuseumSystemApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String greeting() {
+        return "Hello world!";
     }
 
     @Bean
