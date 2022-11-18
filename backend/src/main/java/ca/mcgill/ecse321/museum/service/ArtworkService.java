1a4b0587f7d43b73674fcbf85065bc0bedaefbad
Charles Spencer Blancas <86479293+Charles-Spencer-Blancas@users.noreply.github.com>
2022-11-17 23:56:46 -0500

    Added Javadoc to ArtworkService

--- backend/src/main/java/ca/mcgill/ecse321/museum/service/ArtworkService.java
+++ backend/src/main/java/ca/mcgill/ecse321/museum/service/ArtworkService.java
@@ -22,6 +22,16 @@ public class ArtworkService {
     @Autowired RoomRepository roomRepository;
     @Autowired StorageRoomRepository storageRoomRepository;
 
+    /**
+     * @param title Title of the artwork
+     * @param author Author of the artwork
+     * @param creationDate Date the artwork was created
+     * @param description Description of the artwork
+     * @param imageLink Link to an image of the artwork
+     * @param price Price to loan an artwork
+     * @param isAvailable If the artwork is available to loan
+     * @return A new artwork with the given parameters
+     */
     @Transactional
     public Artwork createArtwork(String title,
                                  String author,
@@ -46,12 +56,19 @@ public class ArtworkService {
         return artworkRepository.save(artwork);
     }
 
+    /**
+     * @param id Id of the artwork to get
+     * @return The artwork if it is found
+     */
     @Transactional public Artwork getArtwork(long id) {
         var artwork = artworkRepository.findById(id).orElse(null);
         if (artwork == null) throw new ServiceLayerException(HttpStatus.NOT_FOUND, "No such artwork");
         return artwork;
     }
 
+    /**
+     * @return List of all artworks in the system
+     */
     @Transactional public List<Artwork> getAllArtworks() { return artworkRepository.findAll();}
 
     @Transactional public void deleteArtwork(long artworkId) {
@@ -60,6 +77,11 @@ public class ArtworkService {
         artworkRepository.delete(artwork);
     }
 
+    /**
+     * @param artworkId Id of the artwork to be moved
+     * @param roomId Id of the room where the artwork is moved
+     * @return The artwork in its new room
+     */
     @Transactional public Artwork moveArtworkToRoom(long artworkId, long roomId) {
         var artwork = artworkRepository.findById(artworkId).orElse(null);
         if (artwork == null) throw new ServiceLayerException(HttpStatus.NOT_FOUND, "No such artwork");
