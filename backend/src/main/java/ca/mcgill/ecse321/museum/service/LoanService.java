72b343847bd2d5622d92acfdb728f65fba49cdd5
Joseph <josephsaliba54@gmail.com>
2022-11-18 03:42:08 -0500

    Fixed loan service and controller such that service has the business logic and controller is a i/o why is this such a long commit message pls Marwan let the pain stop

--- backend/src/main/java/ca/mcgill/ecse321/museum/service/LoanService.java
+++ backend/src/main/java/ca/mcgill/ecse321/museum/service/LoanService.java
@@ -1,13 +1,16 @@
 package ca.mcgill.ecse321.museum.service;
 
 import java.sql.Date;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import ca.mcgill.ecse321.museum.exception.ServiceLayerException;
 import ca.mcgill.ecse321.museum.model.Administrator;
 import ca.mcgill.ecse321.museum.model.Artwork;
 import ca.mcgill.ecse321.museum.model.Loan;
@@ -57,7 +60,9 @@ public class LoanService {
 
     @Transactional
     public Loan getLoan(long id) {
-        return loanRepository.findById(id).orElse(null);
+        Loan loan = loanRepository.findById(id).orElse(null);
+        if (loan == null) throw new ServiceLayerException(HttpStatus.NOT_FOUND, "No such loan");
+        return loan;
     }
 
     @Transactional
@@ -66,7 +71,9 @@ public class LoanService {
     }
 
     @Transactional
-    public List<Loan> getValidatedLoansForArtwork(Artwork artwork) {
+    public List<Loan> getValidatedLoansForArtwork(long artworkId) {
+        Artwork artwork = artworkRepository.findById(artworkId).orElse(null);
+        if (artwork == null) throw new ServiceLayerException(HttpStatus.NOT_FOUND, "No such artwork");
         return loanRepository.findByArtworkAndStatus(artwork.getId(), LoanStatus.VALIDATED);
     }
 
@@ -75,7 +82,8 @@ public class LoanService {
         Loan loan = loanRepository.findById(id).orElse(null);
         Administrator admin = administratorRepository.findById(validatorID).orElse(null);
 
-        if (loan == null || admin == null) return null;
+        if (loan == null) throw new ServiceLayerException(HttpStatus.NOT_FOUND, "No such loan");
+        if (admin == null) throw new ServiceLayerException(HttpStatus.NOT_FOUND, "No such admin");
 
         loan.setValidator(admin);
         loan.setStatus(LoanStatus.VALIDATED);
@@ -87,7 +95,8 @@ public class LoanService {
         Loan loan = loanRepository.findById(id).orElse(null);
         Administrator admin = administratorRepository.findById(validatorID).orElse(null);
 
-        if (loan == null || admin == null) return null;
+        if (loan == null) throw new ServiceLayerException(HttpStatus.NOT_FOUND, "No such loan");
+        if (admin == null) throw new ServiceLayerException(HttpStatus.NOT_FOUND, "No such admin");
 
         loan.setValidator(admin);
         loan.setStatus(LoanStatus.DENIED);
@@ -98,7 +107,13 @@ public class LoanService {
     public Loan requestLoan(long id) {
         Loan loan = loanRepository.findById(id).orElse(null);
 
-        if (loan == null) return null;
+        if (loan == null) throw new ServiceLayerException(HttpStatus.NOT_FOUND, "No such loan");
+
+        List<Loan> validatedLoans = loanRepository.findByArtworkAndStatus(loan.getArtwork().getId(), LoanStatus.VALIDATED);
+
+        for (Loan validatedLoan : validatedLoans)
+            if (loan.getStartDate().before(validatedLoan.getEndDate()) && validatedLoan.getStartDate().before(loan.getEndDate()))
+                throw new ServiceLayerException(HttpStatus.CONFLICT, "Loan conflicts with another validated loan");
 
         loan.setStatus(LoanStatus.PENDING);
         return loanRepository.save(loan);
@@ -107,7 +122,7 @@ public class LoanService {
     @Transactional
     public void requestLoanForAllInCart(long customerID) {
         Visitor customer = (Visitor) personRepository.findById(customerID).orElse(null);
-        if (customer == null) return;
+        if (customer == null) throw new ServiceLayerException(HttpStatus.NOT_FOUND, "No such customer");
 
         List<Loan> inCartLoans = loanRepository.findByCustomerAndStatus(customerID, LoanStatus.INCART);
 
@@ -120,7 +135,7 @@ public class LoanService {
     public void editLoan(long id, Date startDate, Date endDate) {
         Loan loan = loanRepository.findById(id).orElse(null);
 
-        if (loan == null) return;
+        if (loan == null) throw new ServiceLayerException(HttpStatus.NOT_FOUND, "No such loan");
 
         loan.setStartDate(startDate);
         loan.setEndDate(endDate);
