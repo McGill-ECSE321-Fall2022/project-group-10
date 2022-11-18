3d764066dbe27f93720883272f3af3ce71efbc89
Charles Spencer Blancas <86479293+Charles-Spencer-Blancas@users.noreply.github.com>
2022-11-17 23:14:30 -0500

    Moved business logic from ArtworkRestController to ArtworkService

--- backend/src/main/java/ca/mcgill/ecse321/museum/controller/ArtworkRestController.java
+++ backend/src/main/java/ca/mcgill/ecse321/museum/controller/ArtworkRestController.java
@@ -3,6 +3,7 @@ package ca.mcgill.ecse321.museum.controller;
 
 import ca.mcgill.ecse321.museum.controller.body.CreateArtworkRequestBody;
 import ca.mcgill.ecse321.museum.dto.ArtworkDto;
+import ca.mcgill.ecse321.museum.exception.ServiceLayerException;
 import ca.mcgill.ecse321.museum.model.Artwork;
 import ca.mcgill.ecse321.museum.model.ExhibitRoom;
 import ca.mcgill.ecse321.museum.model.Room;
@@ -23,52 +24,62 @@ public class ArtworkRestController {
 
     @PostMapping(value = {"/artworks"})
     public ResponseEntity<ArtworkDto> createArtwork(@RequestBody CreateArtworkRequestBody body) {
-        var artwork = artworkService.createArtwork(
-                body.getTitle(),
-                body.getAuthor(),
-                body.getCreationDate(),
-                body.getDescription(),
-                body.getImageLink(),
-                body.getPrice(),
-                body.isAvailable()
-        );
-        return new ResponseEntity<ArtworkDto>(new ArtworkDto(artwork), HttpStatus.CREATED);
+        try {
+            var artwork = artworkService.createArtwork(
+                    body.getTitle(),
+                    body.getAuthor(),
+                    body.getCreationDate(),
+                    body.getDescription(),
+                    body.getImageLink(),
+                    body.getPrice(),
+                    body.isAvailable()
+            );
+            return new ResponseEntity<ArtworkDto>(new ArtworkDto(artwork), HttpStatus.CREATED);
+        } catch(ServiceLayerException e) {
+            return new ResponseEntity(e.getStatus());
+        }
     }
 
     @GetMapping(value = {"/artworks/{id}"})
     public ResponseEntity<ArtworkDto> getArtwork(@PathVariable long id) throws IllegalArgumentException {
-        var artwork = artworkService.getArtwork(id);
-        return new ResponseEntity<>(new ArtworkDto(artwork), HttpStatus.OK);
+        try {
+            var artwork = artworkService.getArtwork(id);
+            return new ResponseEntity<>(new ArtworkDto(artwork), HttpStatus.OK);
+        } catch(ServiceLayerException e) {
+            return new ResponseEntity(e.getStatus());
+        }
     }
 
     @GetMapping(value = {"/artworks"})
     public ResponseEntity<List<ArtworkDto>> getAllArtworks() throws IllegalArgumentException {
-        var artworks = artworkService.getAllArtworks();
-        var artworkDtos = artworks.stream().map(artwork -> new ArtworkDto(artwork));
-        return new ResponseEntity<List<ArtworkDto>>(artworkDtos.toList(), HttpStatus.OK);
+        try {
+            var artworks = artworkService.getAllArtworks();
+            var artworkDtos = artworks.stream().map(artwork -> new ArtworkDto(artwork));
+            return new ResponseEntity<List<ArtworkDto>>(artworkDtos.toList(), HttpStatus.OK);
+        } catch(ServiceLayerException e) {
+            return new ResponseEntity(e.getStatus());
+        }
     }
 
     @DeleteMapping(value = {"/artworks/{id}"})
     public ResponseEntity deleteArtwork(@PathVariable long id) throws IllegalArgumentException {
-        var artwork = artworkService.getArtwork(id);
-        artworkService.deleteArtwork(artwork);
-        return new ResponseEntity(HttpStatus.OK);
+        try {
+            artworkService.deleteArtwork(id);
+            return new ResponseEntity(HttpStatus.OK);
+        } catch(ServiceLayerException e) {
+            return new ResponseEntity(e.getStatus());
+        }
     }
 
-    @PatchMapping(value = {"/artworks/move/{id}"})
+    @PutMapping(value = {"/artworks/move/{id}"})
     public ResponseEntity<ArtworkDto> moveArtworkToRoom(@PathVariable long id, @RequestParam long roomId) throws IllegalArgumentException {
-        var artwork = artworkService.getArtwork(id);
-        var room = roomService.getRoom(roomId);
-
-        if (artwork == null || room == null) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-
-        if (room instanceof ExhibitRoom exhibitRoom) {
-            if (roomService.countArtworksInExhibitRoom(exhibitRoom) >= (exhibitRoom).getCapacity()) {
-                return new ResponseEntity<>(HttpStatus.FORBIDDEN);
-            }
+        try {
+            var artwork = artworkService.moveArtworkToRoom(id, roomId);
+            return new ResponseEntity<ArtworkDto>(new ArtworkDto(artwork), HttpStatus.OK);
+        } catch(ServiceLayerException e) {
+            return new ResponseEntity(e.getStatus());
         }
-        artwork = artworkService.moveArtworkToRoom(artwork, room);
-        return new ResponseEntity<ArtworkDto>(new ArtworkDto(artwork), HttpStatus.OK);
+
 
     }
 }
