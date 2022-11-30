4cf3eb3b912d09a2896bec32087ae9564c5de98b
Charles Spencer Blancas <86479293+Charles-Spencer-Blancas@users.noreply.github.com>
2022-11-29 23:40:56 -0500

    Frontend skeleton

---
+++ frontend/src/lib/components/dashboard/Navbar.svelte
@@ -0,0 +1,31 @@
+<script>
+  let roles = ["OWNER", "ADMINISTRATOR"] // OWNER, EMPLOYEE, ADMINISTRATOR, VISITOR, USER
+</script>
+
+<div class = "container">
+  {#if roles.includes("ADMINISTRATOR")}
+    <p>Manage artworks</p>
+    <p>Manage requests</p>
+  {/if}
+  {#if roles.includes("OWNER")}
+  <p>Manage schedule</p>
+  <p>Manage employees</p>
+  {/if}
+  {#if roles.includes("EMPLOYEE")}
+  <p>View schedule</p>
+  {/if}
+  {#if roles.includes("VISITOR")}
+  <p>Purchase a pass</p>
+  <p>Browse artworks</p>
+  <p>View my loans</p>
+  <p>Donate an artwork</p>
+  {/if}
+</div>
+
+<style>
+
+  
+  .container {
+    height: 100vh;
+  }
+</style>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,3 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>Landing Page</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <a href="/artworks">See all artworks</a>
+<a href="/dashboard">Go to dashboard</a>
