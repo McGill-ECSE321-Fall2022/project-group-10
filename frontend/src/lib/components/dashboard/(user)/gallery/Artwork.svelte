8bd0ed6343a9f821a77035b8e2be9450693ce1eb
Charles Spencer Blancas <86479293+Charles-Spencer-Blancas@users.noreply.github.com>
2022-12-04 17:49:04 -0500

    #41 Allowed artworks to not be expandable

--- frontend/src/lib/components/dashboard/(user)/gallery/Artwork.svelte
+++ frontend/src/lib/components/dashboard/(user)/gallery/Artwork.svelte
@@ -2,6 +2,7 @@
   import notFound from '$lib/assets/images/not-found.jpg';
 	import { createEventDispatcher } from 'svelte';
   export let artwork;
+  export let expandable = true;
   console.log(artwork);
 
   const imageNotFound = (e) => e.target.src = notFound;
@@ -17,14 +18,14 @@
 </script>
 
 <div class="container">
-  <div class="image-container">
+  <div class="image-container {expandable ? "expandableImage" : ""}">
     <img class="image"
       src="{artwork.imageLink}"
       alt="{artwork.description}"
       on:error={imageNotFound}
       >
   </div>
-  <div class="info">
+  <div class="info {expandable ? "expandableInfo" : ""}">
     <p class="info-text">Title: {artwork.title.length > 10 ? `${artwork.title.slice(0,10)}...` : artwork.title}</p>
     <p class="info-text">Author: {artwork.author.length > 10 ? `${artwork.author.slice(0,10)}...` : artwork.author}</p>
     <p class="click-text" on:click={() => openModal(artwork)}>click to loan/more info</p>
@@ -46,12 +47,15 @@
     padding: 0.5rem;
     border-radius: 14px;
     background-color: white;
+  }
+
+  .expandableImage {
     position: relative;
     top: 0;
     transition: top ease-in-out 0.3s;
   }
 
-  .container:hover .image-container {
+  .container:hover .expandableImage {
     position: relative;
     top: -1rem;
     transition: top ease-in-out 0.3s;
@@ -70,11 +74,14 @@
     border-radius: 14px;
     padding: 0 1rem;
     margin-top: -1rem;
-    transition: all ease-in-out 0.3s;
     margin-bottom: 0;
   }
 
-  .container:hover .info {
+  .expandableInfo {
+    transition: all ease-in-out 0.3s;
+  }
+
+  .container:hover .expandableInfo {
     height: 8rem;
     transition: all ease-in-out 0.3s;
   }
--- frontend/src/routes/dashboard/(admin)/artworks/+page.svelte
+++ frontend/src/routes/dashboard/(admin)/artworks/+page.svelte
@@ -1,1 +1,72 @@
-<h1>Manage artworks</h1>
+<script>
+  import Artwork from "../../../../lib/components/dashboard/(user)/gallery/Artwork.svelte";
+
+  let selectedStorage ;
+  const loadArtworks = async () => {
+    let headers = new Headers();
+	  headers.set('Authorization', `Basic ${btoa('admin@mail.museum.com:admin')}`);
+	  const artworksRes = await fetch('http://localhost:8081/artworks', { headers: headers });
+	  const artworks = await artworksRes.json();
+
+    const storageRoomsRes = await fetch('http://localhost:8081/rooms/storageRoom', { headers: headers });
+    const storageRooms = await storageRoomsRes.json();
+
+    const exhibitRoomsRes = await fetch('http://localhost:8081/rooms/exhibitRoom', { headers: headers });
+    const exhibitRooms = await exhibitRoomsRes.json();
+
+    selectedStorage = storageRooms[0];
+	  return { artworks, storageRooms, exhibitRooms };
+  };
+
+  
+</script>
+
+<div>
+  {#await loadArtworks()}
+  <p>Loading</p>
+  {:then data}
+  <!-- Room Select -->
+    <div class="select-room">
+      <p>Select Current Room</p>
+      <select bind:value={selectedStorage}>
+        <optgroup label="Storage Rooms">
+          {#each data.storageRooms as storageRoom}
+            {console.log(storageRoom.name)}
+            <option value={storageRoom}>
+              {storageRoom.name}
+            </option>
+          {/each}
+        </optgroup>
+
+        <optgroup label="Exhibit Rooms">
+          {#each data.exhibitRooms as exhibitRoom}
+            <option value={exhibitRoom}>
+              {exhibitRoom.name}
+            </option>
+          {/each}
+        </optgroup>
+      </select>
+    </div>
+
+    <!-- Artwork select -->
+    <div class="artworks">
+      {#if data.artworks.length === 0}
+        <p>No artworks yet</p>
+      {:else}
+        {#each data.artworks as artwork}
+          {#if artwork.storage.id == selectedStorage.id}
+            <Artwork {artwork} expandable={false}/>
+          {/if}
+        {/each}
+      {/if}
+    </div>
+  {/await}
+</div>
+
+<style>
+  .artworks {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 8rem;
+  }
+</style>
