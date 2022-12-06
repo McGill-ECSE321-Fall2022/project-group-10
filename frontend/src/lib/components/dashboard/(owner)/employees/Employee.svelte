68e787622b947219ff7f15d713c7a6963705baa8
Al3x-T <alex.turianskyj@gmail.com>
2022-12-05 22:16:11 -0500

    Implemented employee management

---
+++ frontend/src/lib/components/dashboard/(owner)/employees/Employee.svelte
@@ -0,0 +1,62 @@
+<script>
+  //import notFound from '$lib/assets/images/not-found.jpg';
+	import { createEventDispatcher } from 'svelte';
+  export let employee;
+  export let expandable = false;
+  console.log(employee);
+
+  //const imageNotFound = (e) => e.target.src = notFound;
+
+
+  const dispatch = createEventDispatcher();
+
+  const openModal = (employee) => {
+    dispatch('selectEmployee', {
+      selectedEmployee : employee
+    });
+  };
+</script>
+
+<div class="container" on:click={() => openModal(employee)} on:keydown={null}>
+      <!-- on:error={imageNotFound} -->
+  <div class="info {expandable ? "expandableInfo" : ""}">
+    <p class="info-text">{employee.email}</p>
+    <p class="info-text">{employee.firstName} {employee.lastName}</p>
+
+  </div>
+</div>
+
+<style>
+  .container {
+    width: fit-content;
+    height: fit-content;
+    background-color: rgba(63,76,139,.2);
+    border-radius: 14px;
+  }
+
+
+
+  .info {
+    height: 7rem;
+    overflow: hidden;
+    border-radius: 14px;
+    padding: 0.5rem;
+    margin-top: -1rem;
+    transition: all ease-in-out 0.3s;
+    margin-bottom: -1rem;
+  }
+
+
+  .info-text {
+    color: #29346F;
+  }
+
+  .click-text {
+    cursor: pointer;
+    color: #454545;
+    font-size: 0.8rem;
+    text-align: right;
+    margin-top: 2rem;
+  }
+
+</style>
--- frontend/src/routes/dashboard/(owner)/employees/+page.svelte
+++ frontend/src/routes/dashboard/(owner)/employees/+page.svelte
@@ -1,1 +1,82 @@
+<script>
+    import Employee from "../../../../lib/components/dashboard/(owner)/employees/Employee.svelte";
+    import EmployeeModal from "../../../../lib/components/dashboard/(owner)/employees/EmployeeModal.svelte";
+    import {apiCall} from '$lib/scripts/restApi.js'
+
+    $:employees = [];
+    $:selectedEmployee = null;
+    let showEmployeeModal = false;
+
+    const loadEmployees = async () => {
+        //setCredentials('admin@mail.museum.com', 'admin')
+        const employeesRes = await apiCall('GET','administrator/employee');
+        // api call with authentication
+        //const employeesRes = await apiCall('GET','administrator/employee',{email: 'admin@mail.museum.com', password: 'admin'});
+        employees = employeesRes.data;
+        //return employees;
+    };
+
+    const handleSelectedEmployee = (event) => {
+        selectedEmployee = event.detail.selectedEmployee;
+        showEmployeeModal = true;
+    };
+
+    const handleCloseEmployeeModal = () => {
+    showEmployeeModal = false;
+  }
+    
+</script>
+
 <h1>Manage Employees</h1>
+{#if showEmployeeModal}
+    <div class="overlay" on:click={handleCloseEmployeeModal} on:keydown={null}>
+    </div>
+    <EmployeeModal {selectedEmployee} />
+{/if}
+
+
+
+
+<div style="z-index: -5;">
+    {#await loadEmployees()}
+    <p>Loading</p>
+    {:then}
+      <div class="employees">
+        {#if employees.length === 0}
+          <p>No employees yet</p>
+        {/if}
+        {#each employees as employee}
+        <Employee {employee} on:selectEmployee={handleSelectedEmployee}/>
+        {/each}
+      </div>
+    {/await}  
+</div>
+
+<style>
+  .overlay {
+    height: 100%;
+    width: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.2);
+    z-index: 0;
+  }
+
+  .container {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+  }
+  .employees {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 8rem;
+    }
+
+  .employees {
+    background-color: #ECE9FE;
+    padding: 3rem;
+    border-radius: 14px;
+  }
+</style>
